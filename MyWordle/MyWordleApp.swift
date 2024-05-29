5f52e3e2c3322e2b76443b71afb89eea2d01df20
János Székely <szekelyjani@gmail.com>
2024-05-29 12:21:06 +0200

    Initial Commit

---
+++ MyWordle/MyWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWordleApp.swift
+//  MyWordle
+//
+//  Created by János Székely on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
